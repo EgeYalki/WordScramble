7171a794eaa4c54550f308304f38840471e809d3
EgeYalki <egeyalki@hotmail.com>
2025-09-17 13:49:07 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ege Yalki on 17.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
